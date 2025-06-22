68fb07351b3ff42c7610ccf7d5ae19442ede2e77
Nathalie <68371736+nordiskastil@users.noreply.github.com>
2025-06-22 18:47:05 +0200

    Update ThoughtCard.jsx

--- src/components/ThoughtCard.jsx
+++ src/components/ThoughtCard.jsx
@@ -1,1 +1,40 @@
+import React from "react";
 
+export const ThoughtCard = ({ thought, onLike }) => {
+
+  const timeAgo = (dateString) => {
+    let seconds = Math.floor((new Date() - new Date(dateString)) / 1000);
+    const intervals = [
+      [60, "second"],
+      [60, "minute"],
+      [24, "hour"],
+      [7, "day"],
+      [4.35, "week"],
+      [12, "month"],
+      [Number.MAX_SAFE_INTEGER, "year"],
+    ];
+    let i = 0;
+    while (seconds >= intervals[i][0]) {
+      seconds /= intervals[i][0];
+      i++;
+    }
+    const rounded = Math.floor(seconds);
+    return `${rounded} ${intervals[i][1]}${rounded !== 1 ? "s" : ""} ago`;
+  };
+
+  return (
+    <article className="thought-card">
+      <p>{thought.message}</p>
+      <div className="card-footer">
+        <button
+          className={`heart-button ${thought.hearts > 0 ? "liked" : ""}`}
+          onClick={() => onLike(thought._id)}
+        >
+          ❤️
+        </button>
+        <span>x {thought.hearts}</span>
+        <span className="time-ago">{timeAgo(thought.createdAt)}</span>
+      </div>
+    </article>
+  );
+};
